5cf265b973aadb541de24a4384e442dbd07c0934
Sébastien Leroux <sleroux@keep.pt>
2012-11-19 18:29:16 +0000

    Updated description

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_email_notification_content_filter do
   description 'This is a plugin for Redmine that allows to remove the description in the notification emails'
   version '0.0.1'
   author_url 'mailto:sleroux@keep.pt'
-
+  url 'https://github.com/keeps/redmine_email_notification_content_filter.git'
   settings :default => {
     'removeDescriptionFromDocument' => 'false',
     'removeDescriptionFromNews' => 'false',
